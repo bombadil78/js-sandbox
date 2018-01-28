391d2866a7d1e1f5d9643bbc00d5425bfb70bd1e
Christoph Keller <chkeller@gmx.net>
2018-01-28 06:18:52 +0100

    Function bind() and apply() added

---
+++ function-built-in.js
@@ -0,0 +1,26 @@
+function sayHello() {
+    return `hi, my name is ${this.name}`;
+}
+
+function saySomething(x) {
+    return `hi, i am ${this.name} and i say ${x}`;
+}
+
+let obj = {
+    name: "foo"
+};
+
+// apply applies a function after binding its argument to this
+let hi = sayHello.apply(obj);
+console.log(`(1): ${hi}`);
+
+// apply also can take arguments
+let msg = saySomething.apply(obj, [ 'hello' ]);
+console.log(`(2): ${msg}`);
+
+// ... attention: this is dynamically evaluated
+console.log(`(3): ${sayHello()}`);
+
+// bind binds its argument to this and returns the bound function
+let sayHelloBoundToObj = sayHello.bind(obj);
+console.log(`(4): ${sayHelloBoundToObj()}`);
